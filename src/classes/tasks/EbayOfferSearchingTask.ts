9488ea62cdb4bbae65b3c487009ddff17853810a
Justin <montripublic@gmail.com>
2024-11-24 22:55:58 +0100

    rebuild

--- src/classes/tasks/EbayOfferSearchingTask.ts
+++ src/classes/tasks/EbayOfferSearchingTask.ts
@@ -156,6 +156,7 @@ export class EbayOfferSearchingTask extends Task {
             case EbaySortingListBy.NEWEST_OFFER:
                 // only look at offers that are from the last 48 hours.
 
+                // why u dead
                 // @ts-ignore
                 let newerThan48HoursOffers = [];
 
